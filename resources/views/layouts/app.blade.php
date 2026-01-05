1502f909a5474fd15433b1dc8220638121db2a46
Cristhian Daniel <cristhian.lima@multiservicemoc.com.br>
2026-01-05 15:14:44 -0300

    Melhorias

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,7 +18,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css" rel="stylesheet">
 
     {{-- CSS Customizado --}}
-    <link href="{{ asset('css/custom.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('css/custom-prod.css') }}">
 
     @stack('styles')
 </head>
